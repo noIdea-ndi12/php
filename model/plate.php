34668eb57fe1931efaea9ca1985a6da28d0d22fa
Tchaly <>
2012-12-07 06:51:59 +0100

    Controller + Model : PLATE

--- model/plate.php
+++ model/plate.php
@@ -1,75 +1,46 @@
 <?php
-Class ModelPlate {
-        $_nomTable = "plate";
-        
-	function delete($id){
+Class ModelPlate {        
+	function delete($picture_id){
             // TODO : Filtrer l'id ? injection sql ?
+            $rep = "OK";
             
-            // On récupère l'objet de la plaque et :
-            // - les StreetsPictures qui la concerne
-            $this->loadModel('StreetPictures');        
-            if() $rep = $this->StreetPictures->fonctionModel($params);
-        
             // DELETE dans StreetPlatePictures
-            $query_delete = "DELETE FROM plate WHERE plate.id='$id'";
-             
-            
-            // DELETE 
-            // Exécution de la requête
+            $query_delete = "DELETE FROM streets_plates_pictures as plate WHERE plate.pictures_id='$picture_id'";
             if(!_query($query_delete)) return "KO";
+            
+            // DELETE dans StreetPlatePictures
+            $query_delete = "DELETE FROM pictures as plate WHERE pictures.id='$picture_id'";
+            if(!_query($query_delete)) return "KO";
+            
             else return "OK";
 	}
 	
 	function set($params){
 		// R�ponse du model.		
 		$rep = "OK"; 
+               
+                /* $param1 n'est pas de type /like, donc c'est une image
+                par d�finition */
+                $img_to_upload = base64_encode($param2);
+
+                // Upload de l'image 
+                $query_insert_in_pictures = "
+                    INSERT INTO pictures VALUES (null, $img_to_upload)
+                ";
+                if(!_query($query_insert_in_pictures)) $rep = "KO";
+
+                $query_select = "SELECT * FROM pictures ORDER BY id DESC";
+                if(!$row = mysql_fetch_row(_query($query_insert_in_pictures))) $rep = "KO";
+
+                // Je créer le lien image-de-plaque à rue
+                $query_insert_in_pictures = "
+                    INSERT INTO streets_plates_pictures VALUES (".$params[0].", ".$row["id"].", null)
+                ";
+
+                if(!_query($query_insert_in_pictures)) $rep = "KO";
                 
-		if($params[0] == "like"){
-                    
-		}
-		else{
-                    /* $param1 n'est pas de type /like, donc c'est une image
-                    par d�finition */
-                    if($param2){
-                        $img_to_upload = base64_encode($param2);
-                        
-                        // Upload de l'image 
-                        $query_insert_in_pictures = "
-                            INSERT INTO pictures VALUES (null, $img_to_upload)
-                        ";
-                        if(!_query($query_insert_in_pictures)) $rep = "KO";
-                        
-                        // Je créer le lien image-de-plaque à rue
-                        $query_insert_in_pictures = "
-                            INSERT INTO streets_plates_pictures VALUES (".$params[0].", ".$params[].")
-                        ";
-                        if(!_query($query_insert_in_pictures)) $rep = "KO";
-                    }
-		}
                 return $rep;
 	}
-        
-        function get($id){
-            $rep;
-            $query_select =
-            "
-                SELECT * 
-                FROM 
-                    pictures as pic,
-                    streets_plates_pictures as plate                  
-                WHERE 
-                    pic.id = plate.pictures_id 
-                    AND plate.streets_id = '$id'                    
-            ";
-            $result = _query($query_delete);
-            $plaque_de_rue = array();
-            
-            $compteur = 0;
-            while($row = mysql_fetch_array($result){
-                $plaque_de_rue[$compteur]['streets_plates_id'] = ;
-                $compteur++;
-            }
-        }
         
 	function _query($str){
             return mysql_query($str);
