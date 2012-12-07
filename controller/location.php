9e94184e1e0e4364d42305ae61be86fafd5427b6
matthiasP <matthias.posvite@gmail.com>
2012-12-07 03:52:21 +0100

    premiere phase

---
+++ controller/location.php
@@ -0,0 +1,29 @@
+<?php
+
+class Location{
+
+	public function get($id){
+		$select = 'SELECT *';
+		$where = 
+		$res = mysql_query();
+		if($res==null)
+			return echo 'echec';
+		
+	}
+	
+	public function search($page = 0){
+	
+	}
+	
+	public function delete($id){
+	
+	}
+	
+	public function set($id = 0){
+	
+	}
+
+	//public function photo
+}
+
+?>
