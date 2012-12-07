7a0b9421d7aef4af7453975c768dc1e60de8eb97
Tchaly <>
2012-12-07 06:09:03 +0100

    Controller + Model : PLATE

--- model/plate.php
+++ model/plate.php
@@ -1,39 +1,60 @@
-Class Street{
-	
-}
-
+<?php
 Class ModelPlate {
-        $_nomTable = "Plate";
+        $_nomTable = "plate";
         
 	function delete($id){
-            $query_test_exist = sprintf("
-            SELECT * FROM plate
-                     WHERE firstname='%s' AND lastname='%s'",
-                mysql_real_escape_string($firstname),
-                mysql_real_escape_string($lastname));
-    
-            $query_delete = 
-
+            // TODO : Filtrer l'id ? injection sql ?
+            
+            // On récupère l'objet de la plaque et :
+            // - les StreetsPictures qui la concerne
+            $this->loadModel('StreetPictures');        
+            if() $rep = $this->StreetPictures->fonctionModel($params);
+        
+            // DELETE dans StreetPlatePictures
+            $query_delete = "DELETE FROM plate WHERE plate.id='$id'";
+             
+            
+            // DELETE 
             // Exécution de la requête
-            $result = _query($query);
+            if(!_query($query_delete)) return "KO";
+            else return "OK";
 	}
 	
 	function set($param1, $param2){
-		// R�ponse du model.
-		
-		$rep = ; 
+		// R�ponse du model.		
+		$rep = $param; 
 		if($param1 == "like"){
-				
+                    
 		}
 		else{
-			/* $param1 n'est pas de type /like, donc c'est une image
-			par d�finition */
-			if($param2) $img_to_upload = base64_encode($param2);
-			else $rep = "KO";
+                    /* $param1 n'est pas de type /like, donc c'est une image
+                    par d�finition */
+                    if($param2) $img_to_upload = base64_encode($param2);
+                    else $rep = "KO";
 		}		
 	}
-
-	funtion _query($str){
+        
+        function get($id){
+            $rep;
+            $query_select =
+            "
+                SELECT * 
+                FROM 
+                    pictures as pic,
+                    streets_plates_pictures as plate                  
+                WHERE 
+                    pic.id = plate.pictures_id 
+                    AND plate.streets_id = '$id'                    
+            ";
+            $result = _query($query_delete);
+            $plaque_de_rue = array();
+            
+            while($row = mysql_fetch_array($result){
+                $plaque_de_rue[] = ;
+            }
+        }
+        
+	function _query($str){
             return mysql_query($str);
         }
 }
