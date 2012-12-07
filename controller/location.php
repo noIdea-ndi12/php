8606d701d28131d75ff10f8d27a1a1f291274caf
matthiasP <matthias.posvite@gmail.com>
2012-12-07 06:11:28 +0100

    -ajout meth set -ajout meth delete

--- controller/location.php
+++ controller/location.php
@@ -7,14 +7,8 @@ class Location{
 	}
 
 	public function get($id){
-		$res = $this->location->get($id);
-		$location = array(
-				'id' => $location['id'],
-				'name' => $location['name'],
-				'content' => $location['content'],
-				'pictures' => $location['pictures'],
-				);
-		return json_encode($location);
+		$location = $this->location->get($id);
+		return json_encode($location[0]);
 	}
 	
 	public function search($page = 0){
@@ -26,15 +20,33 @@ class Location{
 			$args['name'] = $_POST['name'];
 		if(array_key_exists('city', $_POST) && !empty($_POST['city']))
 			$args['city'] = $_POST['city'];
-		$res = $this->location->get($args['city']);
+		$res = $this->location->get($args);
+		
+		///////////////pagination
+		return json_encode($res);
 	}
 	
 	public function delete($id){
-	
+		$this->location->delete($id);
 	}
 	
 	public function set($id = 0){
-	
+		if(!array_key_exists('id_streets', $_POST) && !array_key_exists('name', $_POST) && !array_key_exists('content', $_POST))
+			exit('erreur => envoyer en POST information id_streets(array), name, content');
+		
+		$args = array();
+		if(array_key_exists('id_streets', $_POST) && !empty($_POST['id_streets']))
+			$args['id_streets'] = $_POST['id_streets'];
+		if(array_key_exists('name', $_POST) && !empty($_POST['name']))
+			$args['name'] = $_POST['name'];
+		if(array_key_exists('content', $_POST) && !empty($_POST['content']))
+			$args['content'] = $_POST['content'];
+		$res = $this->location->get($args);
+
+		if($id==0)
+			$this->location->create($args);
+		else
+			$this->location->update($id, $args);
 	}
 
 	//public function photo
--- modellocation.php
+++ modellocation.php
@@ -19,6 +19,7 @@ class ModelLocation{
 			return array();
 		
 		$res = mysql_query($select.$where);
+		$locations_by_name = array();
 		while($row = mysql_fetch_array($res)){
 			$locations_by_name[$row['id']] = $row;
 		}
@@ -26,7 +27,12 @@ class ModelLocation{
 		$locations = array();
 		foreach($results as $location){
 			$location['pictures'] = $this->getPictures($row['id']);
-			$locations[] = $location;
+			$locations[] = array(
+						'id' => $location['id'],
+						'name' => $location['name'],
+						'content' => $location['content'],
+						'pictures' => $this->getPictures($location['id']),
+						);
 		}
 		return $locations;
 	}
@@ -60,10 +66,95 @@ class ModelLocation{
 		
 		$pictures = array();
 		while($row = mysql_fetch_array($result)){
-			$pictures[] = $row;
+			$pictures[] = array(
+							'id' => $row['pictures.id'],
+							'url' => $row['pictures.url'],
+							'location_id' => $location_id,
+							);
 		}
 		
 		return $pictures;
+	}
+	
+	public function create($args){
+		if(!array_key_exists('id_streets', $args) || !array_key_exists('name', $args) || !array_key_exists('content', $args))
+			return false;
+		
+		$insert = 'INSERT INTO locations';
+		$attributes = ' (name, content)';
+		$values = ' VALUES('."'".$args['name']."','".$args['content']."'".')';
+		mysql_query($insert.$attributes.$values);
+		$location_id = mysqk_insert_id();
+		if($location_id<=0)
+			return false;
+		
+		// $insert = 'INSERT INTO locationsstreet';
+		// $attributes = ' (id_street, location_id)';
+		// $values = '';
+		// foreach($args['id_streets'] as $street_id)
+			// $values.= ' VALUES('.$street_id.','.$location_id.')';
+		$this->assocLocationsStreet($location_id, $args['id_streets']);
+		mysql_query($insert.$attributes.$values);
+		return true;
+	}
+	
+	private function assocLocationsStreet($location_id, $id_streets){
+		$insert = 'INSERT INTO locationsstreet';
+		$attributes = ' (id_street, location_id)';
+		$values = '';
+		foreach($id_streets as $street_id)
+			$values.= ' VALUES('.$street_id.','.$location_id.')';
+		
+		mysql_query($insert.$attributes.$values);
+	}
+	
+	public function update($location_id, $args){
+		$update = 'UPDATE locations';
+		// $set = ' SET';
+		// if(array_key_exists('id_streets', $args))
+			// $set.= ' '$args['id_streets'];
+		$set = array();
+		if(array_key_exists('name', $args))
+			$set[] = 'name = '."'".$args['name']."'";
+		if(array_key_exists('content', $args))
+			$set[] = 'content = '."'".$args['content']."'";
+		$set = ' SET '.implode(',', $set);
+		$where = ' WHERE id='.$location_id;
+		
+		mysql_query($update.$set.$where);
+		
+		$delete = 'DELETE FROM locationsstreet';
+		$where = ' WHERE location_id = '.$location_id;
+		mysql_query($delete.$where);
+		
+		return (mysql_affected_rows()>0);
+	}
+	
+	public function delete($location_id){
+		if(!is_numeric($id))
+			return;
+		$photos = $this->getPictures($location_id);
+		$photos_ids = array();
+		foreach($photos as $photo):
+			$photos_ids[] = $photo['id'];
+		endforeach;
+		
+		//suppression des photos lié à la locations
+		$delete = 'DELETE FROM  pictures';
+		$where = ' WHERE id IN('.implode(',', $photos_ids).')';
+		mysql_query($delete.$where);
+		
+		$delete = 'DELETE FROM  pictures_locations';
+		$where = ' WHERE locations_id = '.$location_id;
+		mysql_query($delete.$where);
+		
+		$delete = 'DELETE FROM locationsstreet';
+		$where = ' WHERE location_id = '.$location_id;
+		mysql_query($delete.$where);
+		
+		$delete = 'DELETE FROM locations';
+		$where = ' WHERE id = '.$location_id;
+		mysql_query($delete.$where);
 	}
 }
 ?>
