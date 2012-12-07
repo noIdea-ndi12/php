edd0c0cb7c3e373f97f07d4159f0fb3cb4f02d99
Simon Blum <simon51100@gmail.com>
2012-12-07 05:57:59 +0100

    Modif
    Modif

--- db.sql
+++ db.sql
@@ -2,202 +2,130 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `noidea` DEFAULT CHARACTER SET latin1 ;
+USE `noidea` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`cities`
+-- Table `noidea`.`cities`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`cities` (
+CREATE  TABLE IF NOT EXISTS `noidea`.`cities` (
   `id` INT NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(255) NULL ,
-  `zip_code` VARCHAR(10) NULL ,
+  `name` VARCHAR(255) NULL DEFAULT NULL ,
+  `zip_code` VARCHAR(10) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`types`
+-- Table `noidea`.`types`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`types` (
+CREATE  TABLE IF NOT EXISTS `noidea`.`types` (
   `id` INT NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(45) NULL ,
+  `name` VARCHAR(45) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`streets`
+-- Table `noidea`.`streets`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`streets` (
+CREATE  TABLE IF NOT EXISTS `noidea`.`streets` (
   `id` INT NOT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(255) NULL ,
-  `abstract` TEXT NULL ,
+  `name` VARCHAR(255) NULL DEFAULT NULL ,
+  `abstract` TEXT NULL DEFAULT NULL ,
   `cities_id` INT NOT NULL ,
   `types_id` INT NOT NULL ,
-  `history` VARCHAR(45) NULL ,
+  `history` VARCHAR(45) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `fk_streets_cities1` (`cities_id` ASC) ,
-  INDEX `fk_streets_types1` (`types_id` ASC) ,
-  CONSTRAINT `fk_streets_cities1`
-    FOREIGN KEY (`cities_id` )
-    REFERENCES `mydb`.`cities` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_streets_types1`
-    FOREIGN KEY (`types_id` )
-    REFERENCES `mydb`.`types` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  INDEX `fk_streets_types1` (`types_id` ASC) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`users`
+-- Table `noidea`.`users`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`users` (
+CREATE  TABLE IF NOT EXISTS `noidea`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT ,
-  `login` VARCHAR(255) NULL ,
-  `pass` VARCHAR(255) NULL ,
-  `type` INT NULL ,
+  `login` VARCHAR(255) NULL DEFAULT NULL ,
+  `pass` VARCHAR(255) NULL DEFAULT NULL ,
+  `type` INT NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`locations`
+-- Table `noidea`.`locations`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`locations` (
+CREATE  TABLE IF NOT EXISTS `noidea`.`locations` (
   `id` INT NOT NULL AUTO_INCREMENT ,
-  `content` TEXT NULL ,
-  `name` VARCHAR(255) NULL ,
-  `gps_x` FLOAT NULL ,
-  `gps_y` FLOAT NULL ,
+  `content` TEXT NULL DEFAULT NULL ,
+  `name` VARCHAR(255) NULL DEFAULT NULL ,
+  `gps_x` FLOAT NULL DEFAULT NULL ,
+  `gps_y` FLOAT NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`comments`
+-- Table `noidea`.`comments`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`comments` (
+CREATE  TABLE IF NOT EXISTS `noidea`.`comments` (
   `id` INT NOT NULL AUTO_INCREMENT ,
-  `date` DATE NULL ,
-  `eval` INT NULL ,
+  `date` DATE NULL DEFAULT NULL ,
+  `eval` INT NULL DEFAULT NULL ,
   `users_id` INT NOT NULL ,
   `streets_id` INT NOT NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `fk_comments_users1` (`users_id` ASC) ,
-  INDEX `fk_comments_streets1` (`streets_id` ASC) ,
-  CONSTRAINT `fk_comments_users1`
-    FOREIGN KEY (`users_id` )
-    REFERENCES `mydb`.`users` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_comments_streets1`
-    FOREIGN KEY (`streets_id` )
-    REFERENCES `mydb`.`streets` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  INDEX `fk_comments_streets1` (`streets_id` ASC) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`pictures`
+-- Table `noidea`.`pictures`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`pictures` (
+CREATE  TABLE IF NOT EXISTS `noidea`.`pictures` (
   `id` INT NOT NULL AUTO_INCREMENT ,
-  `url` VARCHAR(100) NULL ,
+  `url` VARCHAR(100) NULL DEFAULT NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`streets_pictures`
+-- Table `noidea`.`streets_pictures`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`streets_pictures` (
+CREATE  TABLE IF NOT EXISTS `noidea`.`streets_pictures` (
   `streets_id` INT NOT NULL ,
   `pictures_id` INT NOT NULL ,
   PRIMARY KEY (`streets_id`, `pictures_id`) ,
   INDEX `fk_streets_has_pictures_pictures1` (`pictures_id` ASC) ,
-  INDEX `fk_streets_has_pictures_streets` (`streets_id` ASC) ,
-  CONSTRAINT `fk_streets_has_pictures_streets`
-    FOREIGN KEY (`streets_id` )
-    REFERENCES `mydb`.`streets` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_streets_has_pictures_pictures1`
-    FOREIGN KEY (`pictures_id` )
-    REFERENCES `mydb`.`pictures` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  INDEX `fk_streets_has_pictures_streets` (`streets_id` ASC) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`streets_plates_pictures`
+-- Table `noidea`.`streets_plates_pictures`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`streets_plates_pictures` (
+CREATE  TABLE IF NOT EXISTS `noidea`.`streets_plates_pictures` (
   `streets_id` INT NOT NULL ,
   `pictures_id` INT NOT NULL ,
-  `evaluation` VARCHAR(45) NULL ,
+  `evaluation` VARCHAR(45) NULL DEFAULT NULL ,
   PRIMARY KEY (`streets_id`, `pictures_id`) ,
   INDEX `fk_streets_has_pictures_pictures2` (`pictures_id` ASC) ,
-  INDEX `fk_streets_has_pictures_streets1` (`streets_id` ASC) ,
-  CONSTRAINT `fk_streets_has_pictures_streets1`
-    FOREIGN KEY (`streets_id` )
-    REFERENCES `mydb`.`streets` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_streets_has_pictures_pictures2`
-    FOREIGN KEY (`pictures_id` )
-    REFERENCES `mydb`.`pictures` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  INDEX `fk_streets_has_pictures_streets1` (`streets_id` ASC) )
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`pictures_locations`
+-- Table `noidea`.`pictures_locations`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`pictures_locations` (
+CREATE  TABLE IF NOT EXISTS `noidea`.`pictures_locations` (
   `pictures_id` INT NOT NULL ,
   `locations_id` INT NOT NULL ,
   PRIMARY KEY (`pictures_id`, `locations_id`) ,
   INDEX `fk_pictures_has_locations_locations1` (`locations_id` ASC) ,
-  INDEX `fk_pictures_has_locations_pictures1` (`pictures_id` ASC) ,
-  CONSTRAINT `fk_pictures_has_locations_pictures1`
-    FOREIGN KEY (`pictures_id` )
-    REFERENCES `mydb`.`pictures` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_pictures_has_locations_locations1`
-    FOREIGN KEY (`locations_id` )
-    REFERENCES `mydb`.`locations` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `mydb`.`insolites`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`insolites` (
-  `streets_id` INT NOT NULL ,
-  `users_id` INT NOT NULL ,
-  PRIMARY KEY (`streets_id`, `users_id`) ,
-  INDEX `fk_streets_has_users_users1` (`users_id` ASC) ,
-  INDEX `fk_streets_has_users_streets1` (`streets_id` ASC) ,
-  CONSTRAINT `fk_streets_has_users_streets1`
-    FOREIGN KEY (`streets_id` )
-    REFERENCES `mydb`.`streets` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_streets_has_users_users1`
-    FOREIGN KEY (`users_id` )
-    REFERENCES `mydb`.`users` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  INDEX `fk_pictures_has_locations_pictures1` (`pictures_id` ASC) )
 ENGINE = InnoDB;
 
 
